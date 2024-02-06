6640fb0daaf80415797d3034d6761a772e83598d
PabloRM0 <158085295+PabloRM0@users.noreply.github.com>
2024-02-06 13:15:15 +0100

    Merge Practica4_1 Principal.java

--- Principal.java
+++ Principal.java
@@ -1,8 +1,14 @@
+import java.util.Date;
+
 public class Principal {
     public static void main(String[] args) {
         System.out.println("Hola mundo");
 
         String nombre = "Pablo";
         System.out.println("Mi nombre es " + nombre);
+
+        Date fechaActual = new Date();
+
+        System.out.println("La fecha y hora actual es: " + fechaActual);
     }
 }
