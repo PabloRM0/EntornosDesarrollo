37032f56de788d4ac8d28f18741111d9c8f97e8e
PabloRM0 <158085295+PabloRM0@users.noreply.github.com>
2024-02-06 12:54:31 +0100

    Add files via upload

---
+++ Principal.java
@@ -0,0 +1,8 @@
+public class Principal {
+    public static void main(String[] args) {
+        System.out.println("Hola mundo");
+
+        String nombre = "Pablo";
+        System.out.println("Mi nombre es " + nombre);
+    }
+}
