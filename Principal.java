71333c1d38be79a4c3dbbd346b8e54e494ce9568
PabloRM0 <158085295+PabloRM0@users.noreply.github.com>
2024-02-06 13:28:30 +0100

    Añadido método Principal.java

--- Principal.java
+++ Principal.java
@@ -1,7 +1,10 @@
 import java.util.Date;
+import java.util.Scanner;
 
 public class Principal {
     public static void main(String[] args) {
+        Scanner teclado = new Scanner(System.in);
+
         System.out.println("Hola mundo");
 
         String nombre = "Pablo";
@@ -10,5 +13,33 @@ public class Principal {
         Date fechaActual = new Date();
 
         System.out.println("La fecha y hora actual es: " + fechaActual);
+
+        System.out.println("");
+
+        System.out.print("Ingrese un número: ");
+        int numero = teclado.nextInt();
+
+        Usuario[] usuarios = crearUsuarios(numero);
+
+        for (int i = 0; i < usuarios.length; i++) {
+            System.out.println("Introduce usuario " + (i + 1) + ":");
+            System.out.print("Nombre: ");
+            usuarios[i].nombre = teclado.next();
+            System.out.print("Apellidos: ");
+            usuarios[i].apellidos = teclado.next();
+            System.out.print("Email: ");
+            usuarios[i].email = teclado.next();
+        }
+
+        System.out.println("Informacion de los usuarios:");
+        for (Usuario usuario : usuarios) {
+            System.out.println("Nombre: " + usuario.nombre + ", Apellidos: " + usuario.apellidos + ", Email: " + usuario.email);
+        }
+    }
+
+    public static Usuario[] crearUsuarios(int numero) {
+        return new Usuario[numero];
+    }
+
     }
 }
